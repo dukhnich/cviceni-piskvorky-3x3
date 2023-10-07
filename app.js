ee4ba17052554015453d1ccedb09255b0ed3b9fd
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-10-07 09:55:03 +0200

    find winners for all games

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4' // Tento řádek vám umožňuje používat funkci findWinner od jiného programátora. Více se o zápisu s import dozvíte v pozdějších lekcích.
 
-// Kód níže je pouze ukázka použití funkce findWinner. Následující řádky smažte.
+const getEndText = (winner) => {
+	switch (winner) {
+		case 'o': return `Vyhrálo kolečko!`;
+		case 'x': return `Vyhrál křížek!`;
+		case 'tie': return `Remíza!`;
+		default: return `Hra ještě probíhá`;
+	}
+}
 
-// Ukázka použití pro rozehranou hru 3x3, kde začínající hráč kolečko hrál všechny své tři tady do prvního sloupečku a druhý hráč křížek všechny své dva tahy do druhého sloupečku.
-const ukazkoveHerniPole = ['o', 'x', '_', 'o', 'x', '_', 'o', '_', '_']
-// Pro lepší představu stejné pole lze zapsat i takto:
-/*
-const ukazkoveHerniPole = [
-	'o', 'x', '_',
-	'o', 'x', '_',
-	'o', '_', '_',
-]
-*/
-const vitez = findWinner(ukazkoveHerniPole)
-console.log(`Vyhrál hráč se symbolem ${vitez}.`) // 'Vyhrál hráč se symbolem o.'
+const games = document.querySelectorAll('.hra');
+games.forEach(hra => {
+	const policka = hra.querySelectorAll('.policko');
+	const vysledek = hra.querySelector('.vysledek');
+	const gameArr = Array.from(policka).map(el => {
+		if (el.classList.contains('kolecko')) {
+			return 'o';
+		}
+		if (el.classList.contains('krizek')) {
+			return 'x';
+		}
+		return '_';
+	});
+	
+	const winner = findWinner(gameArr);
+	vysledek.textContent = getEndText(winner);
+})
